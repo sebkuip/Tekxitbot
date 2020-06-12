984ad8efd06360b65cd45ebe1b174cdd14fcbc27
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2020-06-12 11:55:38 +0200

    Add kick log to database

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -64,6 +64,33 @@ class moderation(commands.Cog):
         channel = await self.bot.fetch_channel(425632491622105088)
         await channel.send(embed=embed)
 
+    @commands.command(help='Warns the user for the specified reason')
+    @commands.has_permissions(manage_messages=True)
+    async def warn(self, ctx, member: discord.Member, *, reason=None):
+        embed = discord.Embed(color=discord.Color.red())
+        embed.set_author(name=str(member), icon_url=member.avatar_url)
+        embed.add_field(name=f'**YOU GOT WARNED IN', value=f'```{ctx.guild.name}```', inline=False)
+        embed.add_field(name=f'**BY**', value=ctx.author.mention, inline=False)
+        if reason:
+            embed.add_field(name=f'**FOR THE REASON**', value=f'`{reason}`', inline=False)
+        try:
+            await member.send(embed=embed)
+        except discord.Forbidden:
+            await ctx.send('Could not send DM to user')
+        embed.remove_field(0)
+        embed.remove_field(0)
+        embed.insert_field_at(0, name=f'**GOT WARNED BY**', value=ctx.author.mention, inline=False)
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
+        channel = await self.bot.fetch_channel(425632491622105088)
+        await channel.send(embed=embed)
+        try:
+            self.cur.execute(f"INSERT INTO warnings(uid, executor, timedate, reason) VALUES({member.id}, "
+                             f"{ctx.author.id}, CURRENT_TIMESTAMP(1), '{reason}')")
+            self.conn.commit()
+        except Exception as error:
+            print(error)
+
 
 def setup(bot):
     bot.add_cog(moderation(bot))
