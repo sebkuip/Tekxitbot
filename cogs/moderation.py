5b7fc72eee96aef3f2ebbf459577a6aadbc82c2e
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2020-06-12 17:54:36 +0200

    it's warnings not warns

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -97,7 +97,7 @@ class moderation(commands.Cog):
     async def infractions(self, ctx, member: discord.Member):
         try:
             embed = discord.Embed(description='Infractions:')
-            self.cur.execute("SELECT * FROM warns WHERE uid LIKE %s", (member.id,))
+            self.cur.execute("SELECT * FROM warnings WHERE uid LIKE %s", (member.id,))
             warns = self.cur.fetchall()
             self.cur.execute("SELECT * FROM kicks WHERE uid LIKE %s", (member.id,))
             kicks = self.cur.fetchall()
