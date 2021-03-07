85ab0064e8b7ac2372eac0bc16e2679e83d9d100
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2021-03-07 21:43:44 +0100

    Stuff

--- cogs/customcommands.py
+++ cogs/customcommands.py
@@ -7,33 +7,35 @@ class CustomCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot  # This is the bot instance, it lets us interact with most things
 
-    @commands.group(help='Gets the custom help commands', invoke_without_command=True)
-    async def chelp(self, ctx, name='chelp'):
-        name = name.lower()
-        try:
-	@@ -14,9 +16,50 @@ async def chelp(self, ctx, name='chelp'):
-            await ctx.send(f'{message}')
-            await ctx.message.delete()
-
-        except BaseException:
-            pass
+    @commands.group(help='Gets the custom help commands')
+    async def chelp(self, ctx):
+        if ctx.invoked_subcommand is None:
+            name = name.lower()
+            try:
+                with open(f'cogs/chelp/{name}.txt', 'r') as f:
+                    command = f.read()
+                await ctx.send(f'{command}')
+                # await message.delete()
+            except (FileNotFoundError, OSError):
+                pass
 
     @commands.has_permissions(administrator=True)
-    @chelp.command(help='Add a new custom command')
+    @chelp.command()
     async def add(self, ctx, name, *, text):
         name = name.lower()
-        if name in os.listdir('./cogs/chelp'[:-4]):
+        if str(name + '.txt') in os.listdir('./cogs/chelp'):
             await ctx.send(f"Command {name} already exists")
             return
+
         with open(f'cogs/chelp/{name}.txt', 'w') as f:
             f.write(text)
         await ctx.send(f'Created new custom command {name}\n\n{text}')
 
     @commands.has_permissions(administrator=True)
-    @chelp.command(help='Edit an already existing custom command')
+    @chelp.command()
     async def edit(self, ctx, name, *, text):
         name = name.lower()
-        if name not in os.listdir('./cogs/chelp'[:-4]):
+        if str(name + '.txt') not in os.listdir('./cogs/chelp'):
             await ctx.send(f"Command {name} doesn't exists")
             return
         with open(f'cogs/chelp/{name}.txt', 'w') as f:
@@ -41,10 +43,10 @@ class CustomCommands(commands.Cog):
         await ctx.send(f'Edited custom command {name}\n\n{text}')
 
     @commands.has_permissions(administrator=True)
-    @chelp.command(help='Delete a custom command')
+    @chelp.command()
     async def delete(self, ctx, name):
         name = name.lower()
-        if name not in os.listdir('./cogs/chelp'[:-4]):
+        if str(name + '.txt') not in os.listdir('./cogs/chelp'):
             await ctx.send(f"Command {name} doesn't exists")
             return
         await ctx.send(f"Please retype {name} to confirm")
@@ -55,6 +57,7 @@ class CustomCommands(commands.Cog):
             await self.bot.wait_for('message', check=check, timeout=10)
         except TimeoutError:
             await ctx.send("Did not confirm in time. Aborting")
+            return
 
         os.remove(f"./cogs/chelp/{name}.txt")
         await ctx.send(f"sucesfully deleted command {name}")
