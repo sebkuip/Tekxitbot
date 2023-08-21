89f436e2ad99320bb7835fd75b165cf1458eac42
Sebastian Kuipers <kuipers.sebastian@ziggo.nl>
2023-08-21 14:34:31 +0200

    Fixed emojis broken

--- cogs/member.py
+++ cogs/member.py
@@ -29,12 +29,11 @@ class Members(commands.Cog):
 
         # Join message
         await self.joinchannel.send(
-            f"> <:R3:721722986339631234><:R2:721722986159407115><:R4:721722986356539472>**__New "
-            f"Member__**<:R5:721722986373185646><:R3:721722986339631234><:R2:721722986159407115>\n "
-            f"> <:R1:721722971164508190> Welcome {member.mention}!\n"
-            f"> <:R1:721722971164508190> We now have** {self.bot.get_guild(GUILDID).member_count} **members!\n "
-            f"> <:R1:721722971164508190> Be sure to check <#606795102987223051>\n"
-            f"> <:R1:721722971164508190> Hope you enjoy your stay! <:Heart3:655160924134440990>"
+            f"> **__New Member__**\n "
+            f"> Welcome {member.mention}!\n"
+            f"> We now have** {self.bot.get_guild(GUILDID).member_count} **members!\n "
+            f"> Be sure to check <#606795102987223051>\n"
+            f"> Hope you enjoy your stay! <:Heart3:655160924134440990>"
         )
 
     @commands.Cog.listener()
@@ -42,7 +41,7 @@ class Members(commands.Cog):
         # Leave message
         self.joinchannel = member.guild.get_channel(JOINCHANNEL)
         await self.joinchannel.send(
-            f"> <:R6:721990379158896680> ***{member}***  just left the server :slight_frown:"
+            f"> ***{member}***  just left the server :slight_frown:"
         )
 
 
